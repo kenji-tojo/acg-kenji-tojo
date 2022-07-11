614c8105e81074956435fb761b61a073cbe4d97a
kenji-tojo <kenjitojo@icloud.com>
2022-07-11 10:11:28 +0900

    finish task09

--- task09/main.cpp
+++ task09/main.cpp
@@ -43,7 +43,7 @@ Eigen::MatrixXd CppPoissonBlending(
         const double ret_e = ret(ret_i+1, ret_j);
         const double ret_w = ret(ret_i-1, ret_j);
         // write some code here to implement Poisson image editing
-        double ret_c = src_c; // change this line
+        double ret_c = ((4.0 * src_c - src_n - src_s - src_e - src_w) + ret_n + ret_s + ret_e + ret_w) / 4.0;
         // no edit below
         ret_c = (ret_c>255.) ? 255. : ret_c; // clamp
         ret_c = (ret_c<0.) ? 0. : ret_c; // clamp
